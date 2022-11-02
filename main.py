598713fe5513bdbbb92a094217bc72f8c19e9468
phoenix <67361789+lordkilgore@users.noreply.github.com>
2022-11-02 15:52:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2, time
 import mediapipe as mp
+import pyautogui
 
 cam = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
@@ -13,16 +14,26 @@ def findHandPos(capture, color : cv2.cvtColor):
 
 main = True
 while main:
-    time.sleep(1)
+    time.sleep(0.5)
     ret, frame = cam.read()
-    print(findHandPos(frame, cv2.COLOR_BGR2RGB)) 
-    
+    # flips orientation of frame horizontally
+    frame = cv2.flip(frame, 1)
+    coords = findHandPos(frame, cv2.COLOR_BGR2RGB)
 
+    # only calls this when hand is on screen, will prioritize hand that has entered screen most recently
+    if coords != None:
+        # if hand is on the right half of the screen
+        if coords[0].landmark[mpHands.HandLandmark.WRIST].x > 0.5:
+            pyautogui.write('hello')
+        else:
+            pyautogui.write('goodbye')  
+
+    # if camera cannot be captured
     if not ret:
         print("no frame capture")
         break
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('frame', gray)
+    # showing camera & q force exit
+    cv2.imshow('cam', frame)
     if cv2.waitKey(1) == ord('q'):
         break
