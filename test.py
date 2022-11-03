6950dcddaa4bccdb74df3a055100b084eae16878
phoenix <67361789+lordkilgore@users.noreply.github.com>
2022-11-03 13:02:19 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ cam = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
-def findHandPos(capture, color : cv2.cvtColor):
+def findHandPos(capture : cv2.Mat, color : cv2.cvtColor):
     imgColor = cv2.cvtColor(capture, color)
     processed = hands.process(imgColor)
     result = processed.multi_hand_landmarks
@@ -18,17 +18,12 @@ while main:
     frame = cv2.flip(frame, 1)
     coords = findHandPos(frame, cv2.COLOR_BGR2RGB)
     
-    print(coords)
     # only calls this when hand is on screen, will prioritize hand that has entered screen most recently
     if coords != None:
         # if hand is on the right half of the screen
         xcoord = coords[0].landmark[mpHands.HandLandmark.WRIST].x
         ycoord = coords[0].landmark[mpHands.HandLandmark.WRIST].y
         pyautogui.moveTo(1920*xcoord, 1080*ycoord)
-        if xcoord > 0.5:
-            print('hello')
-        else:
-            print('goodbye')  
 
     # if camera cannot be captured
     if not ret:
