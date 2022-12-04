c767fa7024d4f1444cc4e3bc15284b5f8f192d66
phoenix <67361789+lordkilgore@users.noreply.github.com>
2022-12-04 13:25:52 -0800

    Update ServoInput.ino

--- main/ServoInput.ino
+++ main/ServoInput.ino
@@ -1,18 +1,50 @@
 #include <Servo.h>
-Servo myservo; 
-int pos;
-int userInput;
+Servo servo9;
+Servo servo10;
+Servo servo11;
+
+int minPulseWidth = 600;
+int maxPulseWidth = 2400; 
+
+int userInput[3];
+int angle;
+int servo;
+int startbyte;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);
-  myservo.write(0);
+  servo9.attach(9, minPulse, maxPulse);
+  servo10.attach(10, minPulse, maxPulse);
+  servo11.attach(11, minPulse, maxPulse);
+  
+  servo9.write(0);
+  servo10.write(0);
+  servo11.write(0);
 }
 
-void loop()
-{ 
-  if (Serial.available()){
-    userInput = Serial.read();
-    myservo.write(userInput);
-  }
+void loop() {
+  if (Serial.available() > 2) {
+      startbyte = Serial.read();
+
+      if (startbyte == 255) {
+        for (int i = 0; i < 2; i++) {
+          userInput[i] = Serial.read();
+        }
+
+        servo = userInput[0];
+        angle = userInput[1]; 
+
+        switch (servo) {
+          case 9:
+            servo9.write(angle);
+            break;
+           case 10:
+            servo10.write(angle);
+            break;
+           case 11:
+            servo11.write(angle);
+            break;
+        }
+      }
+    }
 }
